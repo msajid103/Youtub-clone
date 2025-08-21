6c879d2782d05ef1a5e0df9090ed7ddd640eb6c0
Muhammad Sajid <msajidnaizi@gmail.com>
2025-08-21 16:49:58 +0500

    Refactor user routes to include additional endpoints for password change, current user retrieval, account updates, and improve code formatting

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { loginUSer, logoutUSer, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    loginUSer,
+    logoutUSer,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+}
+    from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -8,7 +21,7 @@ const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
+            name: "avatar",
             maxCount: 1
         },
         {
@@ -23,11 +36,17 @@ router.route("/login").post(loginUSer)
 
 
 // secured routes
-router.route("/logout").post( verifyJWT, logoutUSer)
+router.route("/logout").post(verifyJWT, logoutUSer)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJWT, upload.single("converImage"), updateUserCoverImage)
+router.route("/channel-details/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
-
-router.route("/test").post( verifyJWT, (req, res)=>{
+router.route("/test").post(verifyJWT, (req, res) => {
     res.send(`Tested: ${req.body.username}`)
 })
 
